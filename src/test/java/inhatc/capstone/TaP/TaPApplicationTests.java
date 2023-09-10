770669b4487b84adeac40b06d0d560734dd677ba
RODONG <jsw26637368@gmail.com>
2023-09-10 14:44:14 +0900

    테스트

--- src/test/java/inhatc/capstone/TaP/TaPApplicationTests.java
+++ src/test/java/inhatc/capstone/TaP/TaPApplicationTests.java
@@ -5,7 +5,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class TaPApplicationTests {
-
+	///테스트의 테스트의 테스트의 테스트의 테스트의 테스트의 테스트의 테스트의 테스트의 테스트의 테스트의 테스트의 테스트의 테스트의 테스트의 테스트의 테스트의 테스트
 	@Test
 	void contextLoads() {
 	}
