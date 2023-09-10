9601a2f084ac8f687ec5e654d5857788cd16f4b2
RODONG <jsw26637368@gmail.com>
2023-09-10 14:26:48 +0900

    테스트

---
+++ src/main/java/inhatc/capstone/TaP/TaPApplication.java
@@ -0,0 +1,13 @@
+package inhatc.capstone.TaP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaPApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaPApplication.class, args);
+	}
+
+}
---
+++ src/test/java/inhatc/capstone/TaP/TaPApplicationTests.java
@@ -0,0 +1,13 @@
+package inhatc.capstone.TaP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaPApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
